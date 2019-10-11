9e8b93434ee5fbca31df79530caefc19011e5c92
HAI <HAI@LAPTOP-H9P5B0OQ>
2019-10-11 09:57:44 +0530

    adding dressestest

---
+++ src/test/java/objects/dressestest.java
@@ -0,0 +1,19 @@
+package objects;
+
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+
+import pages.dressesobject;
+
+public class dressestest {
+	dressesobject dp;
+	
+	public dressestest() {
+		dp=new dressesobject();
+		}
+	
+	
+	
+	
+	
+}
